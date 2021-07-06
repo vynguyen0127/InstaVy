90b7ef6fe95cfbca98a92d294e98e9053bec2d65
Vy Kim Nguyen <vyknguyen@fb.com>
2021-07-06 14:58:30 -0700

    added ability to take a picture and submit

--- app/src/main/java/com/example/instavy/ParseApplication.java
+++ app/src/main/java/com/example/instavy/ParseApplication.java
@@ -3,6 +3,7 @@ package com.example.instavy;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     // Initializes Parse SDK as soon as the application is created
@@ -10,6 +11,8 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("ohAdDnc8RV5dx6qpudkRVJ9ielXOjar3XlYXMSpF")
                 .clientKey("QzthLigb2Xu4JFVVItVoBuoVvj1Ca2F3YOCqUG0N")
